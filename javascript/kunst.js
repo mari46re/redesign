a3a6cc68fbb52cf6c98a87328294bf7843c8256e
Tania Jorgensen <tani1170@stud.kea.dk>
2021-12-14 12:04:37 +0100

    Footer + halv top

---
+++ javascript/kunst.js
@@ -0,0 +1,21 @@
+//Const til knap
+mybutton = document.getElementById("myBtn");
+
+// Når man scroller ned til 20px, fra toppen, dukker knap frem
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// når man klikker på knappen kommer man op til toppen
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox
+}
